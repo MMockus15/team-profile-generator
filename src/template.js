ce2554cc2394315589b59c7e8d8546004864edc6
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-18 19:38:52 -0600

    set up cards in template js

--- src/template.js
+++ src/template.js
@@ -1,34 +1,58 @@
 const generateHTML = (team) => {
 	console.log(team);
-	return   `<!DOCTYPE html>
-	<html lang="en">
-	  <head>
-		<meta charset="UTF-8" />
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<!-- google fonts -->
-		<link rel="preconnect" href="https://fonts.googleapis.com" />
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-		<link
-		  href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Josefin+Sans:wght@400;600&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Noto+Sans+Balinese:wght@400;600&family=Playfair+Display:ital,wght@0,400;0,500;1,400&display=swap"
-		  rel="stylesheet"
-		/>
+	// function to create manager card
+	const createManager = (manager) => {
+		return `<div class="card" style="width: 18rem">
+		<div class="card-body">
+		  <div class="card-header">
+			<h1 class="name">${manager.getName()}</h1>
+			<i class="fa-solid fa-mug-hot m-2"></i>
+			<h3 class="title icon mb-2 text-muted">${manager.getRole()}</h3>
+		  </div>
+		  <p class="id-number">ID#: ${manager.getId()}</p>
+		  <p class="email">Email: ${manager.getEmail()}</p>
+		  <p class="office-number">
+			Office Phone: ${manager.getofficeNumber()}
+		  </p>
+		</div>
+	  </div>`;
+	};
+	// function to create engineer
+	const createEngineer = (engineer) => {
+		return ` <div class="card" style="width: 18rem">
+		<div class="card-body">
+		  <div class="card-header">
+			<h1 class="name">${engineer.getName()}</h1>
+			<i class="fa-solid fa-glasses m-2"></i>
+			<h3 class="title getRole icon mb-2 text-muted">
+			  ${engineer.getRole()}
+			</h3>
+		  </div>
+		  <p class="id-number">ID#: ${engineer.getId()}</p>
+		  <p class="email">Email: ${engineer.getEmail()}</p>
+		  <p class="github">GitHub: ${engineer.getGithub()}</p>
+		</div>
+	  </div>`;
+	};
+	// function to create intern card
+	const createIntern = (intern) => {
+		return `<div class="card" style="width: 18rem">
+		<div class="card-body">
+		  <div class="card-header">
+			<h1 class="name">${intern.getName()}</h1>
+			<i class="fa-solid fa-glasses m-2"></i>
+			<h3 class="title getRole icon mb-2 text-muted">
+			  ${intern.getRole()}
+			</h3>
+		  </div>
+		  <p class="id-number">ID#: ${engineer.getId()}</p>
+		  <p class="email">Email: ${engineer.getEmail()}</p>
+		  <p class="school">School: ${intern.getSchool()}</p>
+		</div>
+	  </div>`;
+	};
 	
-		<!-- bootstrap -->
-		<link
-		  href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-		  rel="stylesheet"
-		  integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-		  crossorigin="anonymous"
-		/>
-		<!-- css style sheet -->
-		<link rel="stylesheet" href="./assets/style.css" />
-		<title>Team Profile Generator</title>
-	  </head>
-	  <body>
-	  <h1>Hello</h1>
-	  </body>
-	</html>`
+	
 }
 
 
