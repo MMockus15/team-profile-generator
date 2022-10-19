307883f49787dd16ece02c8c07e834c5370a67ff
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-19 16:31:40 -0600

    setting up html generator

--- src/template.js
+++ src/template.js
@@ -51,9 +51,74 @@ const generateHTML = (team) => {
 		</div>
 	  </div>`;
 	};
-	
-	
-}
+};
+
+// empty arrays contains generated cards
+const htmlCardArray = [];
+
+// push manager cards to html card array
+htmlCardArray.push(
+	team
+	.filter((employee) => employee.getRole() === "Manager")
+	.map((manager) => createManager(manager))
+	);
+
+// push engineer cards to html card array
+htmlCardArray.push(
+	team
+	.filter((employee) => employee.getRole() === "Engineer")
+	.map((engineer) => createEngineer(engineer))
+	);
+
+// push intern cards to html card array
+htmlCardArray.push(
+	team
+	.filter((employee) => employee.getRole() === "Intern")
+	.map((intern) => createIntern(intern))
+	);
 
 
-  module.exports = generateHTML
+module.exports = (team) => {
+	return `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+	<meta charset="UTF-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<!-- google fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+	href="https://fonts.googleapis.com/css2?family=Cabin:wght@400;700&family=Josefin+Sans:wght@400;600&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Noto+Sans+Balinese:wght@400;600&family=Playfair+Display:ital,wght@0,400;0,500;1,400&display=swap"
+	rel="stylesheet"
+	/>
+	
+	<!-- bootstrap -->
+	<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
+	crossorigin="anonymous"
+	/>
+	<!-- css style sheet -->
+	<link rel="stylesheet" href="./dist/style.css" />
+	<!-- Icons from FontAwesome -->
+	<script
+	src="https://kit.fontawesome.com/d706540582.js"
+	crossorigin="anonymous"
+	></script>
+	<title>Team Profile Generator</title>
+	</head>
+	<body>
+	<header class="team-header">My Team</header>
+	<main>
+	<section class="m-5 d-flex flex-wrap justify-content-around">
+	${generateTeamCards(team)}
+	</section>
+    </main>
+	<script src="./dist/index.js"></script>
+	</body>
+	</html>`;
+};
+
+module.exports = generateHTML;
