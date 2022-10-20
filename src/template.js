432adec0b97aa7b397469cb89f540ae9772ae54e
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-19 18:26:42 -0600

    all prompt questions working and generating

--- src/template.js
+++ src/template.js
@@ -1,8 +1,8 @@
 const generateHTML = (team) => {
-	console.log(team);
-	// function to create manager card
-	const createManager = (manager) => {
-		return `<div class="card" style="width: 18rem">
+  console.log(team);
+  // function to create manager card
+  const createManager = (manager) => {
+    return `<div class="card" style="width: 18rem">
 		<div class="card-body">
 		  <div class="card-header">
 			<h1 class="name">${manager.getName()}</h1>
@@ -16,10 +16,10 @@ const generateHTML = (team) => {
 		  </p>
 		</div>
 	  </div>`;
-	};
-	// function to create engineer
-	const createEngineer = (engineer) => {
-		return ` <div class="card" style="width: 18rem">
+  };
+  // function to create engineer
+  const createEngineer = (engineer) => {
+    return ` <div class="card" style="width: 18rem">
 		<div class="card-body">
 		  <div class="card-header">
 			<h1 class="name">${engineer.getName()}</h1>
@@ -33,10 +33,10 @@ const generateHTML = (team) => {
 		  <p class="github">GitHub: ${engineer.getGithub()}</p>
 		</div>
 	  </div>`;
-	};
-	// function to create intern card
-	const createIntern = (intern) => {
-		return `<div class="card" style="width: 18rem">
+  };
+  // function to create intern card
+  const createIntern = (intern) => {
+    return `<div class="card" style="width: 18rem">
 		<div class="card-body">
 		  <div class="card-header">
 			<h1 class="name">${intern.getName()}</h1>
@@ -45,46 +45,47 @@ const generateHTML = (team) => {
 			  ${intern.getRole()}
 			</h3>
 		  </div>
-		  <p class="id-number">ID#: ${engineer.getId()}</p>
-		  <p class="email">Email: ${engineer.getEmail()}</p>
+		  <p class="id-number">ID#: ${intern.getId()}</p>
+		  <p class="email">Email: ${intern.getEmail()}</p>
 		  <p class="school">School: ${intern.getSchool()}</p>
 		</div>
 	  </div>`;
-	};
+  };
+
+  // empty arrays contains generated cards
+  const htmlCardArray = [];
+
+  // push manager cards to html card array
+  htmlCardArray.push(
+    team
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => createManager(manager))
+  );
+
+  // push engineer cards to html card array
+  htmlCardArray.push(
+  	team
+  	.filter((employee) => employee.getRole() === "Engineer")
+  	.map((engineer) => createEngineer(engineer))
+  	);
+
+  // push intern cards to html card array
+  htmlCardArray.push(
+  	team
+  	.filter((employee) => employee.getRole() === "Intern")
+  	.map((intern) => createIntern(intern))
+  	);
+
+	return htmlCardArray.join("");
 };
 
-// empty arrays contains generated cards
-const htmlCardArray = [];
-
-// push manager cards to html card array
-htmlCardArray.push(
-	team
-	.filter((employee) => employee.getRole() === "Manager")
-	.map((manager) => createManager(manager))
-	);
-
-// push engineer cards to html card array
-htmlCardArray.push(
-	team
-	.filter((employee) => employee.getRole() === "Engineer")
-	.map((engineer) => createEngineer(engineer))
-	);
-
-// push intern cards to html card array
-htmlCardArray.push(
-	team
-	.filter((employee) => employee.getRole() === "Intern")
-	.map((intern) => createIntern(intern))
-	);
-
-
 module.exports = (team) => {
-	return `<!DOCTYPE html>
-	<html lang="en">
-	<head>
-	<meta charset="UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  return `<!DOCTYPE html>
+				<html lang="en">
+				<head>
+				<meta charset="UTF-8" />
+				<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<!-- google fonts -->
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
