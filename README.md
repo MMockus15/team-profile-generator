994dabf2b1c9bc443577b9ee8a5b75b5e12ecf04
MarMarMo <108547874+MMockus15@users.noreply.github.com>
2022-10-21 19:21:25 -0600

    final commit

---
+++ README.md
@@ -0,0 +1,76 @@
+# team-profile-generator
+
+![MIT](https://img.shields.io/badge/License-MIT-yellow)
+
+##Table of Contents
+
+---
+
+1. [Description](#description)
+1. [Installation](#installation)
+1. [Usage](#usage)
+1. [Contribution](#contributing)
+1. [Testing](#tests)
+1. [Additional Info](#additional-info)
+
+## Description:
+
+---
+
+This is a command line web app designed to generate a pre styled webpage that displays my engineering teams basic infomartion so that I can have quick access to their emails and Github profiles.
+
+## Acceptance Criteria:
+
+---
+
+GIVEN a command-line application that accepts user input
+WHEN I am prompted for my team members and their information
+THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+WHEN I click on an email address in the HTML
+THEN my default email program opens and populates the TO field of the email with the address
+WHEN I click on the GitHub username
+THEN that GitHub profile opens in a new tab
+WHEN I start the application
+THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+WHEN I enter the team manager’s name, employee ID, email address, and office number
+THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+WHEN I select the engineer option
+THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+WHEN I select the intern option
+THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+WHEN I decide to finish building my team
+THEN I exit the application, and the HTML is generated
+
+## Installation:
+
+---
+
+Run the index.html in your browser of choice.
+
+## Usage:
+
+---
+
+To use this web app, first clone the repository code into your local device. Then run node index.js in the root folder in the command line. You will be prompted to enter your teams inforamtion. Once you are finished adding in all of your teams info you can select and hit enter on "generate team." This will generate a new index.html with all of your teams information concisely listed on one page.
+
+See the following demo video for any additional guidence you may need. [demo video](https://drive.google.com/file/d/1uL6R8PMP-0RsODbprxr-jM8FjrAsQxC5/view)
+
+## Tests:
+
+---
+
+To test, run npm run test in the root folder. All tests should pass.
+
+## License:
+
+---
+
+[MIT license](https://choosealicense.com/licenses/mit/)
+
+## Questions?:
+
+---
+
+- GitHub: [MMockus15](https://github.com/MMockus15)
+- Email: [marlamockus@gmail.com](marlamockus@gmail.com)
+- Additional Info: Please send message to the email provided above with any additional quetions.
--- index.js
+++ index.js
@@ -6,128 +6,142 @@ var fs = require("fs");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const generateHtml = require("./src/template")
-
-
+const generateHtml = require("./src/template");
 
 const teamArray = [];
 
-
 function createManager() {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your team managers name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your employee ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter your email address.",
-    },
-    {
-      type: "input",
-      name: "officeNumber",
-      message: "What is your office number?",
-    },
-  ]).then(answers => {
-    const manager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-    teamArray.push(manager);
-    createMember();
-  })
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your team managers name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address.",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your office number?",
+      },
+    ])
+    .then((answers) => {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamArray.push(manager);
+      createMember();
+    });
 }
 
 function createMember() {
   inquirer
-  .prompt([
-    {
-      type: "list",
-      name: "choice",
-      message: "Select an engineer, intern, or generate team.",
-      choices: ['engineer', 'intern', 'generate team']
-    },
-  ]).then(answers => {
-    if(answers.choice === 'engineer') {
-      createEngineer();
-    } else if(answers.choice === 'intern') {
-      createIntern();
-    } else {
-      buildTeam();
-    }
-  })
+    .prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "Select an engineer, intern, or generate team.",
+        choices: ["engineer", "intern", "generate team"],
+      },
+    ])
+    .then((answers) => {
+      if (answers.choice === "engineer") {
+        createEngineer();
+      } else if (answers.choice === "intern") {
+        createIntern();
+      } else {
+        buildTeam();
+      }
+    });
 }
 
 function createEngineer() {
-inquirer
-.prompt([
-  {
-    type: "input",
-    name: "name",
-    message: "What is your name?",
-  },
-  {
-    type: "input",
-    name: "id",
-    message: "What is your employee ID?",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Enter your email address.",
-  },
-  {
-    type: "input",
-    name: "github",
-    message: "What is your github name?",
-  },
-]).then(answers => {
-  const engineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-  teamArray.push(engineer);
-  createMember();
-})
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address.",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your github name?",
+      },
+    ])
+    .then((answers) => {
+      const engineer = new Engineer(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.github
+      );
+      teamArray.push(engineer);
+      createMember();
+    });
 }
 
 function createIntern() {
   inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "What is your employee ID?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter your email address.",
-    },
-    {
-      type: "input",
-      name: "school",
-      message: "What is your school?",
-    },
-  ]).then(answers => {
-    const intern = new Intern(answers.name, answers.id, answers.email, answers.school);
-    teamArray.push(intern);
-    createMember();
-  })
-  }
-
-
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your employee ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter your email address.",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is your school?",
+      },
+    ])
+    .then((answers) => {
+      const intern = new Intern(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.school
+      );
+      teamArray.push(intern);
+      createMember();
+    });
+}
 
 function buildTeam() {
-      fs.writeFile("./index.html", generateHtml(teamArray), (err) =>
-      err ? console.log(err) : console.log("Successfully created index.html!")
-    );
+  fs.writeFile("./dist/index.html", generateHtml(teamArray), (err) =>
+    err ? console.log(err) : console.log("Successfully created index.html!")
+  );
 }
 createManager();
